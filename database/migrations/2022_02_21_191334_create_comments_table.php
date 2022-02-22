559ad829e742d693f276e55663a23b71b78130ff
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-02-22 16:07:50 +0330

    Removed extra parent id

--- database/migrations/2022_02_21_191334_create_comments_table.php
+++ database/migrations/2022_02_21_191334_create_comments_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->integer('post_id');
             $table->text('body')->nullable();
-          //  $table->integer('parent_id')->nullable();
             $table->timestamps();
             $table->nestedSet();
         });
